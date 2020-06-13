f50dacc4baf7e3eca8238fca778f0f0455b9f2cc
igibliss00 <igibliss00@gmail.com>
2020-06-12 21:04:04 -0400

    Install Alamofire, SwiftyJSON, and SDWebImage

--- WhatFlower/ViewController.swift
+++ WhatFlower/ViewController.swift
@@ -9,14 +9,17 @@
 import UIKit
 import CoreML
 import Vision
+import Alamofire
+import SwiftyJSON
+import SDWebImage
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+    
+    let wikipediaURL = "https://en.wikipedia.org/w/api.php"
     var pickedImage: UIImage?
-    
     let imagePicker = UIImagePickerController()
-    
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,17 +27,17 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         imagePicker.allowsEditing = true
         imagePicker.sourceType = .camera
     }
-
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         
         if let userPickedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
             guard let convertedCIImage = CIImage(image: userPickedImage) else {
                 fatalError("Could not convert image to CIImage.")
             }
-                        
+            
             detect(flowerImage: convertedCIImage)
             imageView.image = userPickedImage
-
+            
         }
         imagePicker.dismiss(animated: true, completion: nil)
     }
@@ -45,8 +48,11 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         }
         
         let request = VNCoreMLRequest(model: model) { (request, error) in
-            let classification = request.results?.first as? VNClassificationObservation
-            self.navigationItem.title = classification?.identifier
+            guard let classification = request.results?.first as? VNClassificationObservation else {
+                fatalError("Could not classify image")
+            }
+            self.navigationItem.title = classification.identifier.capitalized
+            self.requestInfo(flowerName: classification.identifier)
         }
         
         let handler = VNImageRequestHandler(ciImage: flowerImage)
@@ -54,15 +60,39 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         do {
             try handler.perform([request])
         }
-        
+            
         catch {
             print(error)
         }
+    }
+    
+    func requestInfo(flowerName: String) {
         
+        let parameters: [String:String] = [
+            "format" : "json",
+            "action" : "query",
+            "prop" : "extracts|pageimages",
+            "exintro" : "",
+            "explaintext" : "",
+            "titles" : flowerName,
+            "indexpageids" : "",
+            "redirects" : "1",
+        ]
+        Alamofire.request(wikipediaURL, method: .get, parameters: parameters).responseJSON { (response ) in
+            if response.result.isSuccess {
+                
+                let flowerJSON : JSON = JSON(response.result.value!)
+                let pageid = flowerJSON["query"]["pageids"][0].stringValue
+                let flowerDescription = flowerJSON["query"]["pages"][pageid]["extract"].stringValue
+                let flowerImageURL = flowerJSON["query"]["pages"][pageid]["thumbnail"]["source"].stringValue
+                self.imageView.sd_setImage(with: URL(string: flowerImageURL))
+                
+                self.label.text = flowerDescription
+            }
+        }
     }
     
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
         present(imagePicker, animated: true, completion: nil)
     }
 }
-
